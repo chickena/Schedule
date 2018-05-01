4e723bb1f90e8047ee2272e48db290562e2937e0
mnm234 <minatatsu0373.hornpiano@gmail.com>
2018-05-02 01:35:17 +0900

    Time関係

--- ScheduleV2/app/src/main/java/com/tatsuya/main/schedulev2/TimeActivity.kt
+++ ScheduleV2/app/src/main/java/com/tatsuya/main/schedulev2/TimeActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import kotlinx.android.synthetic.main.activity_time.*
+import kotlinx.android.synthetic.main.fragment_time.*
 import java.util.*
 
 class TimeActivity : AppCompatActivity() {
@@ -14,15 +15,17 @@ class TimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time)
-        val backint: Intent = Intent(this, MainActivity::class.java)
-        ReturnButton.setOnClickListener {
-            startActivity(backint)
-        }
+        val transaction = supportFragmentManager.beginTransaction()
+        val timeFragment = TimeFragment()
 
-        val cal : Calendar = Calendar.getInstance()
-
-        TimeText.text = cal.get(Calendar.HOUR_OF_DAY).toString() + ":" + cal.get(Calendar.MINUTE).toString() + ":" + cal.get(Calendar.SECOND).toString()
+        transaction.replace(R.id.timeFragmentLayout, timeFragment)
+        transaction.commit()
 
 
     }
+
+    fun returnmove() {
+        finish()
+    }
+
 }
---
+++ ScheduleV2/app/src/main/java/com/tatsuya/main/schedulev2/TimeFragment.kt
@@ -0,0 +1,45 @@
+package com.tatsuya.main.schedulev2
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_time.*
+import java.util.*
+
+class TimeFragment : Fragment() {
+
+    lateinit var parent: TimeActivity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val cal: Calendar = Calendar.getInstance()
+        TimeText.text = cal.get(Calendar.HOUR_OF_DAY).toString() + ":" + cal.get(Calendar.MINUTE).toString() + ":" + cal.get(Calendar.SECOND).toString()
+
+        ReturnButton.setOnClickListener { parent.returnmove() }
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parent = context as TimeActivity
+
+    }
+
+
+}
